6f9a72f74c8e20e146f0798f55a6bcf91768baa7
Farrux Madaminov +998937373322 <madaminovfarrux3322@gmail.com>
2023-06-07 00:30:28 +0500

    Splash screen part2 Login screen Logic and ListView, SingleChildScrollView, StatefulWidget part3

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,131 @@
 import 'package:default_project/ui/home/widgets/place_holder.dart';
 import 'package:flutter/material.dart';
-
+bool check = true;
+String phone = "";
+String password = "";
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+  TextEditingController textEditingControllerPassword  = TextEditingController();
+  TextEditingController textEditingControllerPhone  = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
+      home: check ?
+      Scaffold(
         backgroundColor: Colors.white,
         appBar: AppBar(
           backgroundColor: Colors.white,
           elevation: 0,
         ),
-        body: Column(
-          children: [
-              PlaceHolderWidget(title: "CE", titleColor: Colors.blue, color: Colors.black),
-          ],
+        body: Padding(
+          padding:  EdgeInsets.symmetric(horizontal: 16,vertical: 20),
+          child: Column(
+            children: [
+              Text("Login",style: TextStyle(fontSize: 25,fontWeight: FontWeight.w700,color: Colors.black),),
+              SizedBox(height: 30,),
+              TextField(
+                controller: textEditingControllerPhone,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText:  "Phone number",
+                ),
+              ),
+              SizedBox(height: 20,),
+              TextField(
+                controller: textEditingControllerPassword,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText:  "Password",
+                ),
+              ),
+              SizedBox(height: 60,),
+              Container(
+                  width: 200,
+                  height: 50,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(16),
+                    color: Colors.blue,
+                  ),
+                  child: TextButton(onPressed: (){}, child: Text("Next",style: TextStyle(fontSize: 25,fontWeight: FontWeight.w700,color: Colors.black),))),
+              SizedBox(height: 6,),
+              GestureDetector(
+                  onTap: (){
+                    check=!check;
+                    setState(() {
+
+                    });
+
+                  },
+                  child: Text("Registr page",style: TextStyle(fontSize: 18,color: Colors.green),)),
+            ],
+          ),
         ),
-      ),
+      ):
+        Scaffold(
+          backgroundColor: Colors.white,
+          appBar: AppBar(
+            backgroundColor: Colors.white,
+            elevation: 0,
+          ),
+          body: Padding(
+            padding:  EdgeInsets.symmetric(horizontal: 16,vertical: 20),
+            child: Column(
+              children: [
+                Text("Registr",style: TextStyle(fontSize: 25,fontWeight: FontWeight.w700,color: Colors.black),),
+                SizedBox(height: 30,),
+                TextField(
+                  controller: textEditingControllerPhone,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText:  "Phone number",
+                  ),
+                ),
+                SizedBox(height: 20,),
+                TextField(
+                  controller: textEditingControllerPassword,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText:  "Password",
+                  ),
+                ),
+                SizedBox(height: 60,),
+                Container(
+                    width: 200,
+                    height: 50,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(16),
+                      color: Colors.blue,
+                    ),
+                    child: TextButton(onPressed: (){
+                      phone=textEditingControllerPhone.text;
+                      password = textEditingControllerPassword.text;
+                      check=!check;
+
+                      textEditingControllerPhone.dispose();
+                      textEditingControllerPassword.dispose();
+                      setState(() {
+
+                      });
+                    }, child: Text("Next",style: TextStyle(fontSize: 25,fontWeight: FontWeight.w700,color: Colors.black),))),
+
+
+              ],
+            ),
+          )
+        )
     );
   }
 }
--- lib/ui/home/widgets/place_holder.dart
+++ lib/ui/home/widgets/place_holder.dart
@@ -1,24 +1,24 @@
-import 'package:flutter/material.dart';
-
-class PlaceHolderWidget extends StatelessWidget {
-  const PlaceHolderWidget({Key? key, required this.title, required this.titleColor, required this.color}) : super(key: key);
-
-
-  final String title;
-  final Color titleColor;
-  final Color color;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.symmetric(horizontal: 10),
-      height: 64,
-      width: 64,
-      decoration: BoxDecoration(
-        shape: BoxShape.circle,
-        color: color,
-      ),
-      child: ElevatedButton(style: ButtonStyle(backgroundColor: MaterialStateColor) ,onPressed: (){},child: Center(child: Text(title,style: TextStyle(fontSize: 16,fontWeight: FontWeight.w800,color: titleColor),)),),
-    );
-  }
-}
+// import 'package:flutter/material.dart';
+//
+// class PlaceHolderWidget extends StatelessWidget {
+//   const PlaceHolderWidget({Key? key, required this.title, required this.titleColor, required this.color}) : super(key: key);
+//
+//
+//   final String title;
+//   final Color titleColor;
+//   final Color color;
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       margin: EdgeInsets.symmetric(horizontal: 10),
+//       height: 64,
+//       width: 64,
+//       decoration: BoxDecoration(
+//         shape: BoxShape.circle,
+//         color: color,
+//       ),
+//       child: ElevatedButton(style: ButtonStyle(backgroundColor: MaterialStateColor) ,onPressed: (){},child: Center(child: Text(title,style: TextStyle(fontSize: 16,fontWeight: FontWeight.w800,color: titleColor),)),),
+//     );
+//   }
+// }
